a4157b39867ced254a9b8118e5b0bc22acc6213f
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-02-18 22:00:01 +0530

    :sparkles: refactor: each mutation has its own file

--- src/index.js
+++ src/index.js
@@ -1,61 +1,29 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-let links = [{
-    id: 'link-0',
-    description: 'Fullstack tutorial for GraphQL',
-    url: 'www.howtographql.com'
-}]
-
-let idCount = links.length
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 // resolvers
 const resolvers = {
-    Query: {
-        info: () => `This is an API of a hackernews clone`,
-        feeds: () => links
-    },
-    // The first argument, commonly called parent (or sometimes root) is the result of the previous resolver execution level.
-    // Link: {
-    //     id: (parent) => parent.id,
-    //     description: (parent) => parent.description,
-    //     url: (parent) => parent.url, 
-    // },
-
-    Mutation: {
-        createPost: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-
-            links.push(link)
-            return link
-        },
-        editPost: (parent, args) => {
-            links.forEach(link => {
-                if(link.id === args.id) {
-                    link.id = args.id
-                    link.url = args.url
-                    link.description = args.description
-                }
-                return link
-            })
-        },
-        deletePost: (parent, args) => {
-            let index = links.findIndex(link => link.id === args.id)
-            let removedPost = links[index]
-            links.splice(index, 1)
-
-            return removedPost
-        } 
-    }
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 // create a server and start
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: request => { 
+        return {
+            ...request,
+            prisma
+        } 
+    },
 })
 
 server.start(() => console.log('server up and running at localhost:4000'))
